5f5483d1f916c5c85c23d831de48ff4d20186b5d
sb <shekhar.bansal@thalesgroup.com>
2020-07-28 11:01:23 +0530

    added header

--- gremlin-server/src/main/java/org/apache/tinkerpop/gremlin/server/util/GraphTraversalMappingUtil.java
+++ gremlin-server/src/main/java/org/apache/tinkerpop/gremlin/server/util/GraphTraversalMappingUtil.java
@@ -30,7 +30,7 @@ import java.util.concurrent.ConcurrentHashMap;
 /**
  * Utility class for obtaining graph traversal name to graph name
  *
- * @author shekhar.bansals
+ * @author shekhar.bansal
  */
 public class GraphTraversalMappingUtil {
 
